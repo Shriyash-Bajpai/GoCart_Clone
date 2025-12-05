e335072bbc1543f3aae642c0c40d5c379b03ca9e
ShriyashBajpai <shriyashbajpai@gmail.com>
2025-12-06 00:12:14 +0530

    Add the create-store apis Trial 2

--- configs/imageKit.js
+++ configs/imageKit.js
@@ -2,6 +2,9 @@ import ImageKit from '@imagekit/nodejs';
 
 export const client = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+  
 });
 
 export default client;
